c5ee37803ca3cb39a45be83bd0dae4093322cc00
tiansh <tiansheng111@gmail.com>
2014-07-23 10:57:03 +0800

    meta.js & README

---
+++ Tieba_Client/Tieba_Client.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name Tieba Client
+// @namespace http://tieba.baidu.com
+// @include http://tieba.baidu.com/*
+// @updateURL https://firefoxbar.github.io/userscripts/Tieba_Client/Tieba_Client.meta.js
+// @downloadURL https://firefoxbar.github.io/userscripts/Tieba_Client/Tieba_Client.user.js
+// @icon http://imgsrc.baidu.com/forum/pic/item/8a65d04bd11373f0075bca22a60f4bfbfaed04c9.jpg
+// @require http://libs.baidu.com/jquery/2.0.0/jquery.min.js
+// @grant GM_xmlhttpRequest
+// @author 网络中二行客
+// @version 0.9.1
+// ==/UserScript==
+
