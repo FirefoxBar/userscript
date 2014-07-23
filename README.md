4f1861c9c2ff18e4ed5d97257501c2c59f04bf70
linusyu <ilikethisgames@gmail.com>
2014-07-23 23:00:20 +0800

    change updateURL

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # userscripts #
 
-[Tieba Client](https://github.com/FirefoxBar/userscripts/blob/gh-pages/Tieba_Client/)
-
+[【点击安装】](https://github.com/FirefoxBar/userscript/raw/master/Tieba_Client/Tieba_Client.user.js) [Tieba Client](https://github.com/FirefoxBar/userscript/tree/master/Tieba_Client)
--- Tieba_Client/Tieba_Client.meta.js
+++ Tieba_Client/Tieba_Client.meta.js
@@ -2,8 +2,8 @@
 // @name Tieba Client
 // @namespace http://tieba.baidu.com
 // @include http://tieba.baidu.com/*
-// @updateURL https://raw.githubusercontent.com/FirefoxBar/userscript/gh-pages/Tieba_Client/Tieba_Client.meta.js
-// @downloadURL https://github.com/FirefoxBar/userscript/raw/gh-pages/Tieba_Client/Tieba_Client.user.js
+// @updateURL https://github.com/FirefoxBar/userscript/raw/master/Tieba_Client/Tieba_Client.meta.js
+// @downloadURL https://github.com/FirefoxBar/userscript/raw/master/Tieba_Client/Tieba_Client.user.js
 // @icon http://imgsrc.baidu.com/forum/pic/item/8a65d04bd11373f0075bca22a60f4bfbfaed04c9.jpg
 // @require http://libs.baidu.com/jquery/2.0.0/jquery.min.js
 // @grant GM_xmlhttpRequest
