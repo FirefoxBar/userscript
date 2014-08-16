3aca0da4104e334cafe2f12b8ad8b2677d0e21af
linusyu <ilikethisgames@gmail.com>
2014-08-16 19:52:49 +0800

    add Tieba Private
    add Tieba Private

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 #用户脚本列表#
 
 * [【点击安装】](https://github.com/FirefoxBar/userscript/raw/master/Tieba_Client/Tieba_Client.user.js) [Tieba Client](https://github.com/FirefoxBar/userscript/tree/master/Tieba_Client)
-* [【点击安装】](https://github.com/FirefoxBar/userscript/raw/master/Tieba_Quick_Reply/Tieba_Quick_Reply.user.js) [ieba Quick Reply](https://github.com/FirefoxBar/userscript/tree/master/Tieba_Quick_Reply)
+* [【点击安装】](https://github.com/FirefoxBar/userscript/raw/master/Tieba_Quick_Reply/Tieba_Quick_Reply.user.js) [Tieba Quick Reply](https://github.com/FirefoxBar/userscript/tree/master/Tieba_Quick_Reply)
+* [【点击安装】](https://github.com/FirefoxBar/userscript/raw/master/Tieba_Private.user.js/Tieba_Private.user.js) [Tieba Quick Reply](https://github.com/FirefoxBar/userscript/tree/master/Tieba_Private)
---
+++ Tieba_Private/Tieba_Private.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name Tieba Private
+// @namespace http://tieba.baidu.com
+// @include http://tieba.baidu.com/home/*
+// @updateURL https://github.com/FirefoxBar/userscript/raw/master/Tieba_Private.user.js/Tieba_Private.user.js.meta.js
+// @downloadURL https://github.com/FirefoxBar/userscript/raw/master/Tieba_Private.user.js/Tieba_Private.user.js.user.js
+// @version 1
+// @grant none
+// @author      网络中二行客
+// ==/UserScript==
+
+;(function ($) {
+    $.ajax({
+        type: 'post',
+        url: '/home/post/delvisite',
+        data: {
+            ie: 'utf-8',
+            un: $('.userinfo_username').text(),
+            tbs: unsafeWindow.PageData.tbs
+        },
+        dataType: 'json'
+    }).success(function (t) {
+        0 === t.no && $('.visitor_card:first').animate({
+            width: 0
+        }, 200, function () {
+            $('.visitor_card:first').remove(),
+            (function () {
+                var t = $('#visitor_card_wrap') .find('.visitor_card').length;
+                0 === t && $('#visitor_card_wrap') .closest('.ihome_visitor').slideUp(200)
+            }) ();
+        })
+    })
+})(unsafeWindow.$);
