77b18e09ec1681ab02bcf4df47d825a87d7a7441
linusyu <ilikethisgames@gmail.com>
2014-08-16 19:55:15 +0800

    Tieba Private
    Tieba Private

--- Tieba_Private/README.md
+++ Tieba_Private/README.md
@@ -1,6 +1,6 @@
 # Tieba Private #
 
-* [【点击安装】](https://github.com/FirefoxBar/userscript/raw/master/Tieba_Private.user.js/Tieba_Private.user.js)
+* [【点击安装】](https://github.com/FirefoxBar/userscript/raw/master/Tieba_Private/Tieba_Private.user.js)
 * author: 网络中二行客
 * 尾行新体验
 * version: 1
--- Tieba_Private/Tieba_Private.user.js
+++ Tieba_Private/Tieba_Private.user.js
@@ -2,8 +2,8 @@
 // @name Tieba Private
 // @namespace http://tieba.baidu.com
 // @include http://tieba.baidu.com/home/*
-// @updateURL https://github.com/FirefoxBar/userscript/raw/master/Tieba_Private.user.js/Tieba_Private.user.js.meta.js
-// @downloadURL https://github.com/FirefoxBar/userscript/raw/master/Tieba_Private.user.js/Tieba_Private.user.js.user.js
+// @updateURL https://github.com/FirefoxBar/userscript/raw/master/Tieba_Private/Tieba_Private.meta.js
+// @downloadURL https://github.com/FirefoxBar/userscript/raw/master/Tieba_Private/Tieba_Private.user.js
 // @version 1
 // @grant none
 // @author      网络中二行客
