d77ee5d81a682ad463433aa024a3ab8e927f081e
shuangya <896640547@qq.com>
2014-11-19 18:24:03 +0800

    更新删帖机器人

--- Post_Del_Robot/PostDelRobot.meta.js
+++ Post_Del_Robot/PostDelRobot.meta.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Post Del Robot
 // @namespace    http://blog.sylingd.com
-// @version      4
+// @version      5
 // @description  删帖机器人
 // @author       ShuangYa
 // @match        http://tieba.baidu.com/f?*
 // @grant        GM_xmlhttpRequest
 // @grant        GM_registerMenuCommand
 // @grant        GM_addStyle
+// @grant        GM_setClipboard
 // @run-at		 document-end
 // @require		 http://libs.baidu.com/jquery/1.11.1/jquery.min.js
 // @updateURL 	 https://github.com/FirefoxBar/userscript/raw/master/Post_Del_Robot/PostDelRobot.meta.js
