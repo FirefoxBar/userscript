be55ef8098eb343ffbb2239aebecc0bd225721d2
shuangya <896640547@qq.com>
2014-12-10 12:53:49 +0800

    更新删帖机器人

--- Post_Del_Robot/PostDelRobot.user.js
+++ Post_Del_Robot/PostDelRobot.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
-// @name         Post Del Robot
-// @namespace    http://blog.sylingd.com
-// @version      5
-// @description  删帖机器人
-// @author       ShuangYa
-// @match        http://tieba.baidu.com/f?*
-// @grant        GM_xmlhttpRequest
-// @grant        GM_registerMenuCommand
-// @grant        GM_addStyle
-// @grant        GM_setClipboard
-// @run-at		 document-end
-// @require		 http://libs.baidu.com/jquery/1.11.1/jquery.min.js
+// @name Post Del Robot
+// @namespace http://blog.sylingd.com
+// @version 6
+// @description 删帖机器人
+// @author ShuangYa
+// @include http://tieba.baidu.com/f?*
+// @grant GM_xmlhttpRequest
+// @grant GM_registerMenuCommand
+// @grant GM_addStyle
+// @grant GM_setClipboard
+// @run-at document-end
+// @require http://libs.baidu.com/jquery/1.11.1/jquery.min.js
 // @updateURL 	 https://github.com/FirefoxBar/userscript/raw/master/Post_Del_Robot/PostDelRobot.meta.js
 // @downloadURL  https://github.com/FirefoxBar/userscript/raw/master/Post_Del_Robot/PostDelRobot.user.js
 // ==/UserScript==
@@ -145,7 +145,6 @@
 		if (me.type == 1) { //回贴
 			postdata.pid = me.pid;
 		} else if (me.type == 2) { //主题
-		} else if (me.type == 3) { //楼中楼
 		}
 		//GM的ajax不知道为什么不能用，就用jQuery的吧
 		$.ajax({
@@ -156,18 +155,26 @@
 				// 确认删帖结果
 				result = eval('(' + response + ')');
 				if (result.err_code == 0) {
-					logResult('删除贴子成功！主题ID：' + me.tid);
+					if (me.type == 1) { //回贴
+						logResult('删除贴子成功！主题ID：' + me.tid + '，贴子ID：' + me.pid);
+					} else if (me.type == 2) { //主题
+						logResult('删除贴子成功！主题ID：' + me.tid);
+					}
 					if (num != leng) { //调用自身
 						delByList(num + 1);
 					}
 				} else {
 					console.log(postdata);
 					console.log(result);
-					logResult('删除贴子失败！主题ID：' + me.tid);
+					if (me.type == 1) { //回贴
+						logResult('删除贴子失败！<a href="http://tieba.baidu.com/p/' + me.tid + '?pid=' + me.pid + '&cid=1#1" target="_blank">查看</a>');
+					} else if (me.type == 2) { //主题
+						logResult('删除贴子失败！<a href="http://tieba.baidu.com/p/' + me.tid + '" target="_blank">查看</a>');
+					}
 					if (result.err_code == '224011') {
-						logResult('触发验证码，停止删帖（解决办法：刷新当前网页）');
+						logResult('触发验证码，停止删帖（解决办法：刷新当前网页，或手动删除一个贴子）');
 						GM_setClipboard(meinput);
-						logResult('提示：本次用户ID/关键词已经复制到剪贴板');
+						logResult('本次用户ID/关键词已经复制到剪贴板');
 						Stop();
 						$('#sy_del_reload').show();
 					} else {
@@ -236,7 +243,7 @@
 	};
 	//注册菜单
 	if (window.location.href.match(/f\?(.*?)kw=/) !== null) { //确认是在贴子列表页
-		GM_addStyle('#sy_del_full{position:fixed;background:rgba(0,0,0,0.6);width:100%;height:100%;top:0;left:0;color:white;z-index:99998;}#sy_del_dialog{position:fixed;z-index:99999;width:460px;height:400px;top:10px;left:50%;margin-left:-200px;}#sy_del_info{height:295px;padding:10px;}');
+		GM_addStyle('#sy_del_full{position:fixed;background:rgba(0,0,0,0.6);width:100%;height:100%;top:0;left:0;color:white;z-index:99998;}#sy_del_dialog{position:fixed;z-index:99999;width:460px;height:400px;top:10px;left:50%;margin-left:-200px;}#sy_del_info{height:295px;padding:10px;overflow-y:scroll;}');
 		$('body').append('<div id="sy_del_full"></div><div id="sy_del_dialog" class="dialogJ dialogJfix dialogJshadow ui-draggable m_dialog"><div class="uiDialogWrapper"><div style="-moz-user-select: none;" class="dialogJtitle"><span class="dialogJtxt">删帖机器人</span></div><div class="dialogJcontent"><div class="dialogJbody"><div id="sy_del_info"></div><div class="m_dialog_container j_m_dialog_container"><div class="m_button_panel clearfix"><a id="sy_del_close" class="ui_btn ui_btn_sub_m j_m_btn_cancel m_btn_cancel"><span><em>关闭</em></span></a><a id="sy_del_reload" class="ui_btn ui_btn_m j_m_btn_insert m_btn_insert" onclick="window.location.reload();"><span><em>立即刷新</em></span></a></div></div></div></div></div></div>');
 		$('#sy_del_dialog').hide();
 		$('#sy_del_full').hide();
