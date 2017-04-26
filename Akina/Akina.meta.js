422cfe2241e8923531738fa521dd39eed2df58ef
轮回眼的鸣人 <lhydmr@gmail.com>
2017-04-26 15:29:49 +0800

    Update Akina.meta.js

--- Akina/Akina.meta.js
+++ Akina/Akina.meta.js
@@ -5,7 +5,7 @@
 // @downloadURL https://github.com/FirefoxBar/userscript/raw/master/Akina/Akina.user.js
 // @include     /https?:\/\/tieba\.baidu\.com\/p.*/
 // @include     /https?:\/\/tieba\.baidu\.com\/f.*/
-// @version     2.0.3
+// @version     2.0.4
 // @grant       GM_xmlhttpRequest
 // @author      Paltoo Young
 // ==/UserScript==
