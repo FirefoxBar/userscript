4250bd3ed6fce225703b5164f9fb14a8cb0de196
ShuangYa <896640547@qq.com>
2018-04-03 11:50:45 +0800

    修复部分网站的HTTPS支持

--- Reading_Mode/Reading_Mode.user.js
+++ Reading_Mode/Reading_Mode.user.js
@@ -4,6 +4,7 @@
 // @description       阅读模式
 // @author            ShuangYa
 // @include           http://blog.csdn.net/*/article/details/*
+// @include           https://blog.csdn.net/*/article/details/*
 // @include      	  http://*.cnblogs.com/*/p/*
 // @include     	  http://*.cnblogs.com/*/archive/*
 // @include      	  http://*.cnblogs.com/*/articles/*
@@ -14,6 +15,7 @@
 // @include      	  http://*.blog.163.com/blog/static/*
 // @include      	  http://*.blog.sohu.com/*
 // @include      	  http://blog.tianya.cn/post-*
+// @include      	  https://blog.tianya.cn/post-*
 // @include      	  http://blog.sciencenet.cn/blog-*
 // @include      	  http://blog.chinaunix.net/uid-*
 // @grant			  GM_setValue
@@ -21,7 +23,7 @@
 // @run-at            document-end
 // @updateURL         https://github.com/FirefoxBar/userscript/raw/master/Reading_Mode/Reading_Mode.meta.js
 // @downloadURL       https://github.com/FirefoxBar/userscript/raw/master/Reading_Mode/Reading_Mode.user.js
-// @version           9
+// @version           10
 // ==/UserScript==
 
 (function() {
