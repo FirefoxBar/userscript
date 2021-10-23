7f99515f6f67607c87b3eb8e9bd959a77c6234a5
ShuangYa <i@sylingd.com>
2021-10-23 18:05:42 +0800

    feat: update CSDN

--- Fxxk_CSDN/Fxxk_CSDN.user.js
+++ Fxxk_CSDN/Fxxk_CSDN.user.js
@@ -1,4 +1,15 @@
-GM_addStyle('#csdn-redpack, .toolbar-advert { display: none !important; }');
+GM_addStyle([
+  '#csdn-redpack, .toolbar-advert, #overTheScreen, .hljs-button.signin, #articleSearchTip',
+  '{ display: none !important }',
+  '#content_views pre, #content_views pre code {',
+  '-webkit-touch-callout: default !important;',
+  '-webkit-user-select: auto !important;',
+  '-khtml-user-select: auto !important;',
+  '-moz-user-select: auto !important;',
+  '-ms-user-select: auto !important;',
+  'user-select: auto !important;',
+  '}'
+].join(''));
 
 const hide = document.querySelector('.hide-article-box');
 if (hide) {
