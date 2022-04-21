6be8aa5df2261a7b715f63e703cb2363332596f2
ShuangYa <i@sylingd.com>
2022-04-21 20:01:40 +0800

    feat: 增加 LoadBT batch download

---
+++ LoadBT_batch_download/LoadBT_batch_download.user.js
@@ -0,0 +1,121 @@
+(function () {
+  let _fetchQueueCount = 0;
+  const _fetchQueueArr = [];
+  const _fetchQueueRun = async () => {
+    const item = _fetchQueueArr.shift();
+    if (!item) return;
+    fetch(item.url)
+      .then((x) => x.json())
+      .then(item.resolve)
+      .catch(item.reject)
+      .finally(() => {
+        _fetchQueueCount--;
+        _fetchQueueRun();
+      });
+  };
+  const fetchQueue = (url) => {
+    return new Promise((resolve, reject) => {
+      _fetchQueueArr.push({ url, resolve, reject });
+      if (_fetchQueueCount < 3) {
+        _fetchQueueCount++;
+        _fetchQueueRun();
+      }
+    });
+  };
+
+  const waitUntil = (cb) => {
+    return new Promise((resolve) => {
+      const check = () => {
+        setTimeout(() => {
+          if (cb()) resolve();
+          else check();
+        }, 300);
+      };
+      check();
+    });
+  };
+
+  async function onItemClick(folderId) {
+    const el = document.createElement("div");
+    el.style.position = "fixed";
+    el.style.zIndex = "99999";
+    el.style.textAlign = "center";
+    el.style.bottom = "20px";
+    el.style.left = "50%";
+    el.style.width = "400px";
+    el.style.maxWidth = "100%";
+    el.style.transform = "translateX(-50%)";
+    el.style.border = "1px solid #00000026";
+    el.style.borderRadius = "0.25rem";
+    el.style.padding = "12px";
+    el.style.background = "#fff";
+    el.innerHTML = "<div class='desc'></div><div class='progress'><div class='progress-bar bg-info'></div></div>";
+    document.body.appendChild(el);
+
+    el.querySelector(".desc").innerText = "正在获取文件列表";
+
+    const { files } = await fetchQueue("/files/" + folderId);
+    const result = [];
+    for (const file of files) {
+      const u = await fetchQueue("/download/" + file.id);
+      result.push(u.url);
+      // 更新进度
+      el.querySelector(".desc").innerText = result.length + " / " + files.length;
+      el.querySelector(".progress-bar").style.width = (100 * result.length) / files.length + "%";
+    }
+
+    el.remove();
+    GM_setClipboard(result.join("\n"));
+
+    setTimeout(() => alert("已复制"));
+  }
+
+  async function run() {
+    // 加载文件夹列表
+    const folders = (await fetchQueue("/files/0")).files;
+    if (folders.length === 0) {
+      alert("没有可下载的文件夹");
+      return;
+    }
+
+    const folderList = document.createElement("div");
+    folderList.className = "dropdown-menu dropdown-menu-right show";
+    folderList.style.position = "fixed";
+    folderList.style.zIndex = "99999";
+    folderList.style.top = "50%";
+    folderList.style.left = "50%";
+    folderList.style.width = "400px";
+    folderList.style.maxWidth = "100%";
+    folderList.style.transform = "translate(-50%, -50%)";
+
+    folders.forEach((folder) => {
+      const item = document.createElement("span");
+      item.classList = "dropdown-item";
+      item.innerText = folder.name;
+      item.style.cursor = "pointer";
+      item.setAttribute("data-id", folder.id);
+
+      item.addEventListener("click", () => {
+        onItemClick(folder.id);
+        folderList.remove();
+      });
+
+      folderList.appendChild(item);
+    });
+
+    document.body.appendChild(folderList);
+  }
+
+  async function main() {
+    await waitUntil(() => !!document.getElementById("torrent-file"));
+    const container = document.getElementById("torrent-file").parentElement;
+    const b = document.createElement("button");
+    b.type = "button";
+    b.className = "btn btn-primary mb-2 ml-2 d-none d-sm-block";
+    b.innerHTML = "批量下载";
+    b.addEventListener("click", run);
+    container.appendChild(b);
+  }
+
+  main();
+})();
---
+++ LoadBT_batch_download/README.md
@@ -0,0 +1,7 @@
+# LoadBT batch download
+
+LoadBT 批量复制下载链接，适用网站：https://www.loadbt.com/
+
+[【点击安装】](https://userscript.firefoxcn.net/js/LoadBT_batch_download.user.js)
+
+author: 泷涯
