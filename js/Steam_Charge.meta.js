972407c6460dd70c40fb06a8025dbb92e74f0cbf
sylingd <896640547@qq.com>
2022-04-21 16:49:28 +0000

    Deploying to gh-pages from @ FirefoxBar/userscript@4e2cc3eb2277627a55673adbd7ed9c1982e9213c 🚀

--- js/Steam_Charge.meta.js
+++ js/Steam_Charge.meta.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name Steam自定义充值
+// @name Steam 自定义充值
 // @namespace https://blog.sylingd.com
-// @description 自行输入一次性往Steam充值多少
+// @description 自行输入一次性往 Steam 钱包充值数量
 // @author ShuangYa
-// @version 1.0.0
+// @version 1.0.1
 // @match https://store.steampowered.com/steamaccount/addfunds*
 // @updateURL https://userscript.firefoxcn.net/js/Steam_Charge.meta.js
 // @downloadURL https://userscript.firefoxcn.net/js/Steam_Charge.user.js
