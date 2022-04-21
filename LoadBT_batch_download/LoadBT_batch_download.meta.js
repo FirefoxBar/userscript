63b76b7b4a6e3ee39ee119a4fac3ae93fc24d859
sylingd <896640547@qq.com>
2022-04-21 12:03:02 +0000

    Deploying to master from @ FirefoxBar/userscript@6be8aa5df2261a7b715f63e703cb2363332596f2 🚀

---
+++ LoadBT_batch_download/LoadBT_batch_download.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name LoadBT batch download
+// @version 1
+// @description LoadBT 批量复制下载链接
+// @include https://www.loadbt.com/files*
+// @author ShuangYa
+// @run-at document-end
+// @icon https://www.loadbt.com/images/icons/favicon.ico
+// @grant unsafeWindow
+// @grant GM_setClipboard
+// @namespace http://blog.sylingd.com
+// @updateURL https://userscript.firefoxcn.net/js/LoadBT_batch_download.meta.js
+// @downloadURL https://userscript.firefoxcn.net/js/LoadBT_batch_download.user.js
+// ==/UserScript==
