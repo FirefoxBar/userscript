26a8fc524a84fc5ec2d8237afa09407d82d38a9c
sylingd <896640547@qq.com>
2022-04-21 16:49:26 +0000

    Deploying to master from @ FirefoxBar/userscript@4e2cc3eb2277627a55673adbd7ed9c1982e9213c 🚀

--- Steam_Charge/Steam_Charge.user.js
+++ Steam_Charge/Steam_Charge.user.js
@@ -1,17 +1,40 @@
 // ==UserScript==
-// @name Steam自定义充值
+// @name Steam 自定义充值
 // @namespace https://blog.sylingd.com
-// @description 自行输入一次性往Steam充值多少
+// @description 自行输入一次性往 Steam 钱包充值数量
 // @author ShuangYa
-// @version 1.0.0
+// @version 1.0.1
 // @match https://store.steampowered.com/steamaccount/addfunds*
 // @updateURL https://userscript.firefoxcn.net/js/Steam_Charge.meta.js
 // @downloadURL https://userscript.firefoxcn.net/js/Steam_Charge.user.js
 // ==/UserScript==
-document.getElementById('input_currency').type = "text";
-document.getElementById('input_amount').type = "text";
-document.getElementById('input_amount').value = "3000";
-var el = document.createElement('input');
-el.type = "submit";
-el.value = "充值（单位为分）";
-document.getElementById('form_addfunds').appendChild(el);
+(function () {
+  const container = document.getElementById("prices_user");
+
+  // 获取转换率
+  const firstItem = container.querySelector(".addfunds_area_purchase_game");
+  const newItem = firstItem.cloneNode(true);
+  const displayAmount = newItem
+    .querySelector(".game_purchase_price")
+    .innerText.trim();
+  newItem.querySelector(".game_purchase_price").remove();
+  const submit = newItem.querySelector("[data-currency]");
+  const realAmount = submit.getAttribute("data-amount");
+
+  // 填入输入框
+  const t = Array.from(newItem.children).find((x) => x.tagName === "H1");
+  const tips = Array.from(newItem.children).find((x) => x.tagName === "P");
+
+  t.innerHTML = "<input class='j_text' type='text' style='height:30px' />";
+  tips.innerHTML = realAmount + " = " + displayAmount;
+
+  // 提交
+  submit.removeAttribute("onclick");
+  submit.addEventListener("click", () => {
+    document.getElementById("input_amount").value = t.querySelector("input").value;
+    document.getElementById("input_currency").value = submit.getAttribute("data-currency");
+    document.getElementById("form_addfunds").submit();
+  });
+
+  container.insertBefore(newItem, container.children[0]);
+})();
