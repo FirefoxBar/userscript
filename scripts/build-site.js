eb34b2ac643ec3cea35211955a7e4000a4adadcf
泷涯 <i@sylingd.com>
2022-04-22 16:55:11 +0800

    feat: add icon for api.json

--- scripts/build-site.js
+++ scripts/build-site.js
@@ -1,40 +1,40 @@
 const { existsSync, readdirSync, mkdirSync, statSync } = require("fs");
 const { copySync, writeFile } = require("fs-extra");
-const { resolve } = require("path");
+const path = require("path");
 const buildOne = require("./utils/build-one");
 const copyOne = require("./utils/copy-one");
 const chalk = require("chalk");
 
-const root = resolve(__dirname, "..");
+const root = path.join(__dirname, "..");
 const dirs = readdirSync(root);
 
-if (!existsSync(resolve(root, "dist"))) {
-  mkdirSync(resolve(root, "dist"));
+if (!existsSync(path.join(root, "dist"))) {
+  mkdirSync(path.join(root, "dist"));
 }
-if (!existsSync(resolve(root, "dist/master"))) {
-  mkdirSync(resolve(root, "dist/master"));
+if (!existsSync(path.join(root, "dist/master"))) {
+  mkdirSync(path.join(root, "dist/master"));
 }
-if (!existsSync(resolve(root, "dist/pages"))) {
-  mkdirSync(resolve(root, "dist/pages"));
+if (!existsSync(path.join(root, "dist/pages"))) {
+  mkdirSync(path.join(root, "dist/pages"));
 }
 
 // 生成列表
 async function main() {
   // 编译脚本
-  const distJs = resolve(root, "dist/pages/js");
+  const distJs = path.join(root, "dist/pages/js");
   if (!existsSync(distJs)) {
     mkdirSync(distJs);
   }
   const queue = dirs.map(async (it) => {
-    const fullPath = resolve(root, it);
+    const fullPath = path.join(root, it);
     if (!statSync(fullPath).isDirectory()) {
       return;
     }
     // meta.yml
-    if (existsSync(resolve(fullPath, "package.json"))) {
+    if (existsSync(path.join(fullPath, "package.json"))) {
       // 需要编译的
       return await buildOne(it, distJs);
-    } else if (existsSync(resolve(root, it, it + ".user.js"))) {
+    } else if (existsSync(path.join(root, it, it + ".user.js"))) {
       // 纯复制，但仍然需要解析meta
       return copyOne(it, distJs);
     }
@@ -45,39 +45,36 @@ async function main() {
     const list = result
       .map((it) => {
         if (!it) return;
-        console.log(
-          chalk.green("✔") +
-            " 已" +
-            (it.stats ? "编译" : "复制") +
-            " " +
-            it.name
-        );
+        console.log(chalk.green("✔") + it.name + ": 已" + (it.stats ? "编译" : "复制"));
         const meta = it.meta.meta;
-        return {
+        const res = {
           name: meta.name,
           version: meta.version,
           installURL: meta.downloadURL,
-          homepageURL:
-            "https://github.com/FirefoxBar/userscript/tree/main/" + it.name,
+          homepageURL: "https://github.com/FirefoxBar/userscript/tree/main/" + it.name,
           description: meta.description,
         };
+        if (meta.icon) {
+          res.icon = meta.icon;
+        }
+        return res;
       })
       .filter((x) => !!x)
       .sort((a, b) => a.name.localeCompare(b.name));
 
-    if (!existsSync(resolve(root, "dist/pages/api"))) {
-      mkdirSync(resolve(root, "dist/pages/api"));
+    if (!existsSync(path.join(root, "dist/pages/api"))) {
+      mkdirSync(path.join(root, "dist/pages/api"));
     }
     await writeFile(
-      resolve(root, "dist/pages/api/list.json"),
+      path.join(root, "dist/pages/api/list.json"),
       JSON.stringify(list)
     );
     await writeFile(
-      resolve(root, "dist/pages/api/list.js"),
+      path.join(root, "dist/pages/api/list.js"),
       "onGetList(" + JSON.stringify(list) + ")"
     );
     // 复制站点文件
-    copySync(resolve(__dirname, "www"), resolve(root, "dist/pages"));
+    copySync(path.join(__dirname, "www"), path.join(root, "dist/pages"));
   } catch (err) {
     if (Array.isArray(err)) {
       err.forEach((error) => {
