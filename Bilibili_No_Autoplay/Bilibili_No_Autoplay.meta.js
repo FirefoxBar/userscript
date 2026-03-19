d9ca497838a08ce184608565fed34ced420936d3
sylingd <896640547@qq.com>
2026-03-19 16:43:57 +0000

    Deploying to master from @ FirefoxBar/userscript@5a49f7394e8c3957c7c519e3cc1eef58a646d935 🚀

---
+++ Bilibili_No_Autoplay/Bilibili_No_Autoplay.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name Bilibili No Autoplay
+// @version 1
+// @description 禁用B站视频播放完后自动下一集
+// @include https://www.bilibili.com/*
+// @author ShuangYa
+// @namespace http://blog.sylingd.com
+// @updateURL https://userscript.firefoxcn.net/js/Bilibili_No_Autoplay.meta.js
+// @downloadURL https://userscript.firefoxcn.net/js/Bilibili_No_Autoplay.user.js
+// ==/UserScript==
