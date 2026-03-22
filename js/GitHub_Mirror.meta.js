68650b1fc0b5733b91c22b9da975a28d4a28a2f2
sylingd <896640547@qq.com>
2026-03-22 05:35:03 +0000

    Deploying to gh-pages from @ FirefoxBar/userscript@ac5970e2e43c32d0e2cd1a0cf48fa0648982b6c7 🚀

---
+++ js/GitHub_Mirror.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name GitHub Mirror
+// @version 1
+// @description 自动替换 GitHub 页面上的相关链接到镜像站
+// @include https://github.com/*
+// @author ShuangYa
+// @run-at document-end
+// @icon https://github.githubassets.com/favicons/favicon.png
+// @grant unsafeWindow
+// @grant GM_registerMenuCommand
+// @grant GM_setValue
+// @grant GM_getValue
+// @namespace blog.sylingd.com
+// @updateURL https://userscript.firefoxcn.net/js/GitHub_Mirror.meta.js
+// @downloadURL https://userscript.firefoxcn.net/js/GitHub_Mirror.user.js
+// ==/UserScript==
